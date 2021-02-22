8a8df3aaaf26292710d11faa7d99644baf35bd77
AhmedMojahed <asaad0861@gmail.com>
2021-02-22 22:47:38 +0200

    create resourse group and network

--- main.tf
+++ main.tf
@@ -13,3 +13,23 @@ provider "azurerm" {
 }
 
 
+# Create a resource group
+resource "azurerm_resource_group" "testgroup" {
+  name     = "test-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "testnetwork" {
+  name                = "test-network"
+  resource_group_name = azurerm_resource_group.testgroup.name
+  location            = azurerm_resource_group.testgroup.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "testsubnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.testgroup.name
+  virtual_network_name = azurerm_virtual_network.testnetwork.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
